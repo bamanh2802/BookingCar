3d520a33183f4afd3549dfe0961b4e989ecdfeec
quycot2k3@gmail.com <jizero2003@gmail.com>
2025-06-30 12:21:33 +0700

    fix cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -9,24 +9,18 @@ const WHITELIST_DOMAINS = ['http://localhost:5173', 'http://localhost:3000', 'ht
 //config dynamic cors
 export const corsOptions = {
   origin: function (origin, callback) {
-    // Cho phép việc gọi API bằng POSTMAN trên môi trường dev,
-    // Thông thường khi sử dụng postman thì cái origin sẽ có giá trị là undefined
-    if (!origin && env.BUILD_MODE === 'dev') {
+    if (!origin) {
       return callback(null, true)
     }
-
-    // Kiểm tra xem origin có phải là domain được chấp nhận hay không
+    
     if (WHITELIST_DOMAINS.includes(origin)) {
-      return callback(null, true)
+      return callback(null, origin) 
     }
 
-    // Cuối cùng nếu domain không được chấp nhận thì trả về lỗi
-    return callback(new ApiError(StatusCodes.FORBIDDEN, `${origin} not allowed by our CORS Policy.`))
+    return callback(new ApiError(StatusCodes.FORBIDDEN, `${origin} không được phép bởi CORS Policy của chúng tôi.`))
   },
 
-  // Some legacy browsers (IE11, various SmartTVs) choke on 204
   optionsSuccessStatus: 200,
 
-  // CORS sẽ cho phép nhận cookies từ request
   credentials: true
 }
