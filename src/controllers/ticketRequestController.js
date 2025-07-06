24ab779ac663f222235b248f6dc3bd4e96476a36
son <sonnguyentk269@gmail.com>
2025-07-06 14:50:56 +0700

    fix: lấy danh sách trip theo giờ hiện tại/ lấy danh sách ticket/ticketRequest của  user dưới agent2

--- src/controllers/ticketRequestController.js
+++ src/controllers/ticketRequestController.js
@@ -29,6 +29,49 @@ const getTicketRequests = catchAsync(async (req, res) => {
   })
   if (req.user.roleName === USER_ROLES.AGENT_LV2) {
     // Nếu là AGENT_LV2, chỉ lấy vé do chính user đó tạo
+
+    const getUserList = await userService.getUsersCreatedByParent(req.user._id)
+    const userList = getUserList.results
+    if (!userList || userList.length === 0) {
+      filter.createdBy = req.user._id
+      filter.userId = req.user._id
+    }
+
+    const userIds = userList.map((user) => user._id)
+    filter.createdBy = req.user._id
+    filter.userId = [req.user._id, ...userIds]
+  } else if (req.user.roleName === USER_ROLES.AGENT_LV1) {
+    // Nếu là AGENT_LV1, lấy vé do chính user đó tạo và các AGENT_LV2 do user này quản lý
+    // Giả sử có hàm userService.getAgentLv2ByAgentLv1Id trả về danh sách AGENT_LV2 do AGENT_LV1 này quản lý
+    const getAgentLv2List = await userService.getUsersCreatedByParent(req.user._id)
+    const agentLv2List = getAgentLv2List.results
+    if (!agentLv2List || agentLv2List.length === 0) {
+      // Nếu không có AGENT_LV2 nào, chỉ lấy vé do chính user đó tạo
+      filter.createdBy = req.user._id
+    }
+    const agentLv2Ids = agentLv2List.map((agent) => agent._id)
+    filter.createdBy = [req.user._id, ...agentLv2Ids]
+    filter.userId = [req.user._id, ...agentLv2Ids]
+  } else if (req.user.roleName === USER_ROLES.CLIENT) {
+    filter.userId = req.user._id
+  }
+  const ticketRequests = await ticketRequestService.getTicketRequests(filter, parseInt(page), parseInt(limit))
+  return res.status(StatusCodes.OK).json(ApiResponse.success(ticketRequests, 'Lấy danh sách yêu cầu vé thành công'))
+})
+/**
+ * Lấy danh sách yêu cầu vé tạo hộ
+ */
+const getTicketRequests1 = catchAsync(async (req, res) => {
+  const { page = 1, limit = 10, ...restQuery } = req.query
+  let filter = req.query.filter || {}
+
+  // Hỗ trợ truyền trực tiếp nhiều trường filter qua query string
+  const allowedFields = ['status', 'userId', 'tripId', 'createdAt', 'updatedAt', 'tittleRequest']
+  allowedFields.forEach((field) => {
+    if (restQuery[field]) filter[field] = restQuery[field]
+  })
+  if (req.user.roleName === USER_ROLES.AGENT_LV2) {
+    // Nếu là AGENT_LV2, chỉ lấy vé do chính user đó tạo
     filter.createdBy = req.user._id
     filter.userId = req.user._id
   } else if (req.user.roleName === USER_ROLES.AGENT_LV1) {
